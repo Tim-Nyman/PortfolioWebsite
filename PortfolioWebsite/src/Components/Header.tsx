e7835a49e82e2dc0792bbcb16b7395dd5b477280
Tim Nyman <timnyman@live.com>
2025-03-07 02:45:24 +0100

    Added all the logos & progress bar based on skill in the "skills"-section

--- PortfolioWebsite/src/Components/Header.tsx
+++ PortfolioWebsite/src/Components/Header.tsx
@@ -1,31 +1,47 @@
-import React from 'react';
-import { Navbar, Container, Nav, Col, NavDropdown, Row } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Navbar, Container, Nav, Col, NavDropdown, Row, Button } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
+
 
 const Header: React.FC = () => {
+
+    useEffect(() => {
+        document.body.setAttribute("data-bs-spy", "scroll");
+        document.body.setAttribute("data-bs-target", "#navbar-nav");
+        document.body.setAttribute("data-bs-offset", "50");
+        document.body.classList.add("scrollspy-example");
+    }, []);
+
     return (
         <header>
-            <Container fluid>
+            <Container>
                 <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="top" className='navbar-shadow'>
-                    <Navbar.Brand href="#start">
-                        <Row>
-                            <Col>
-                                <img src="Images/Code blocks.png" alt="Logo" className="me-1" />
-                                Tim Nyman
-                            </Col>
-                        </Row>
-                    </Navbar.Brand>
+                    <Container>
+                        <Navbar.Brand href="#start">
+                            <Row className="custom-padding">
+                                <Col>
+                                    <h2>
+                                        Tim Nyman
+                                    </h2>
+                                </Col>
+
+                                <Col className="section-col">
+                                    <Button href="https://github.com/Tim-Nyman" target="_blank" className="header-button">
+                                        <Image src="Images/Logos/GitHub.png" className="button-img"></Image>
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Navbar.Brand>
+                    </Container>
 
                     <Navbar.Toggle aria-controls="navbar-nav" />
-
                     <Navbar.Collapse id="navbar-nav">
                         <Nav className="ms-auto">
                             <Nav.Link href="#start">0. Start</Nav.Link>
-                            <Nav.Link href="#about">1. About</Nav.Link>
-                            <Nav.Link href="#skills">2. Skills</Nav.Link>
-                            <Nav.Link href="#Portfolio">3. Portfolio</Nav.Link>
-                            <Nav.Link href="#Testimonials">4. Testimonials</Nav.Link>
-                            <Nav.Link href="#Contact">5. Contact</Nav.Link>
-                            <Nav.Link href="Resume/2024 CV Tim.pdf" target="_blank">6. Resume</Nav.Link>
+                            <Nav.Link href="#skills">1. Skills</Nav.Link>
+                            <Nav.Link href="#portfolio">2. Portfolio</Nav.Link>
+                            <Nav.Link href="#contact">3. Contact</Nav.Link>
+                            <Nav.Link href="Resume/2024 CV Tim.pdf" target="_blank">4. Resume</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
--- PortfolioWebsite/src/Components/Section.tsx
+++ PortfolioWebsite/src/Components/Section.tsx
@@ -1,56 +1,176 @@
-import { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Container, Row, Col, Button, Tab, ListGroup, TabContent, Nav, Card } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image'
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import AnimatedProgressBar from './AnimatedProgressBar';
+
 
 function Example() {
-  // const [show, setShow] = useState(false);
 
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
+  const [activeTab, setActiveTab] = useState<string>('#Backend');
+
+  const handleTabSelect = (selectedKey: string | null) => {
+    if (selectedKey) {
+      setActiveTab(selectedKey);
+    }
+  };
 
   return (
     <>
-      <section id="start" className="dark-section">
-        <Container>
-          <Row>
-            <Col>
-              <Image src="Images/Profile Picture.png" className="rounded-circle" alt="Tim's Portrait" />
+      <div className="scroll-container">
+        <section id="start" className="dark-section">
+          <Container>
+            <Row>
+              <Col>
+                <Image src="Images/Profile/Profile Picture.png" className="rounded-circle" alt="Tim's Portrait" />
+              </Col>
+
+              <Col className="section-col">
+                <h2>Hi! I am Tim,</h2>
+                <h2>a C# & .NET developer in the making</h2>
+                <p>
+                  .NET & C# Student - Always learning, Always improving.
+                </p>
+                <Button onClick={() => window.open('/CV Tim 2025.pdf', '_blank')} variant="primary outline-secondary" className="button-section">
+                  Resume
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+
+        <section id="skills" className="dark-section">
+          <Container>
+            <Tab.Container id="list-group-skills" activeKey={activeTab} onSelect={handleTabSelect}    >
+              <Row>
+                <Col sm={4} className="col-custom">
+                  <ListGroup>
+                    <ListGroup.Item action href="#Backend" className="custom-list-item">
+                      Backend
+                    </ListGroup.Item>
+
+                    <ListGroup.Item action href="#Frontend" className="custom-list-item">
+                      Frontend
+                    </ListGroup.Item>
+
+                    <ListGroup.Item action href="#Database" className="custom-list-item">
+                      Database
+                    </ListGroup.Item>
+
+                    <ListGroup.Item action href="#Cloud" className="custom-list-item">
+                      Cloud and Tools
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+
+                <Col sm={8} className="col-custom">
+                  <TabContent className="custom-pane">
+                    <Tab.Pane eventKey="#Backend">
+                      <Image src="Images/Logos/CSharp.png" className="pane-image-backend"></Image>
+                      <AnimatedProgressBar now={70} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Backend">
+                      <Image src="Images/Logos/dot-net-core.png" className="pane-image-backend"></Image>
+                      <AnimatedProgressBar now={54} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Backend">
+                      <Image src="Images/Logos/asp.png" className="pane-image-backend"></Image>
+                      <AnimatedProgressBar now={24} />
+                    </Tab.Pane>
+                  </TabContent>
+
+                  <Tab.Content className="custom-pane">
+                    <Tab.Pane eventKey="#Frontend">
+                      <Image src="Images/Logos/React.png" className="pane-image"></Image>
+                      <AnimatedProgressBar now={65} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Frontend">
+                      <Image src="Images/Logos/TypeScript.png" className="pane-image"></Image>
+                      <AnimatedProgressBar now={50} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Frontend">
+                      <Image src="Images/Logos/JavaScriptFinal.png" className="pane-image"></Image>
+                      <AnimatedProgressBar now={50} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Frontend">
+                      <Image src="Images/Logos/Bootstrap2.png" className="pane-image"></Image>
+                      <AnimatedProgressBar now={60} />
+                    </Tab.Pane>
+                  </Tab.Content>
+
+
+                  <TabContent className="custom-pane">
+                    <Tab.Pane eventKey="#Database">
+                      <Image src="Images/Logos/azure.png" className="pane-image-sql"></Image>
+                      <AnimatedProgressBar now={24} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Database">
+                      <Image src="Images/Logos/MSSQL.png" className="pane-image-sql"></Image>
+                      <AnimatedProgressBar now={70} />
+                    </Tab.Pane>
+                  </TabContent>
+
+                  <TabContent className="custom-pane">
+                    <Tab.Pane eventKey="#Cloud">
+                      <Image src="Images/Logos/Microsoft_Azure_Logo.png" className="pane-image-cloud"></Image>
+                      <AnimatedProgressBar now={34} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Cloud">
+                      <Image src="Images/Logos/Azure DevOps.png" className="pane-image-cloud"></Image>
+                      <AnimatedProgressBar now={18} />
+                    </Tab.Pane>
+
+                    <Tab.Pane eventKey="#Cloud">
+                      <Image src="Images/Logos/GitHub.png" className="pane-image-cloud"></Image>
+                      <AnimatedProgressBar now={60} />
+                    </Tab.Pane>
+                  </TabContent>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </Container>
+        </section>
+
+        <section id="portfolio" className="dark-section">
+          <Container>
+            <Col className="col-custom">
+              <Row>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="Images/Profile/Profile Picture.png" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Row>
+
+              <Row>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="Images/Profile/Profile Picture.png" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the
+                      bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Row>
             </Col>
-
-            <Col>
-              <h2>Hi! I am Tim,</h2>
-              <h2>a C# & .NET developer in the making</h2>
-              <p>
-                .NET & C# Student - Always learning, Always improving.
-              </p>
-            </Col>
-            
-          </Row>
-        </Container>
-      </section>
-
-
-
-
-
-
-      {/* <Container>
-        <Button variant="primary" onClick={handleShow}>
-          Launch
-        </Button>
-
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            Some text as placeholder. In real life you can have the elements you
-            have chosen. Like, text, images, lists, etc.
-          </Offcanvas.Body>
-        </Offcanvas>
-      </Container> */}
+          </Container>
+        </section >
+      </div>
     </>
   );
 }
