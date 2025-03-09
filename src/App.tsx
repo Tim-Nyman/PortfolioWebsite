ab1f83fb8548c8f1d348ffec627ad664fbbad8c6
Tim Nyman <timnyman@live.com>
2025-03-09 02:40:56 +0100

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Header from './Components/Header'
-import Footer from "./Components/Footer"
 import Section from "./Components/Section"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
